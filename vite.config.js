0e34dfc911331e21fffaf1cba5d9ce966aa7cf93
faith816-del <faithhosea816@gmail.com>
2025-10-20 20:45:23 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/week3-react-tailwind/', // <-- add this
+  base: '/week3-react-tailwind/' // This must exactly match your repo name
 })
