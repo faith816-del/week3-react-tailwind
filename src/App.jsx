5ce1bee5d13eb0fe92ebba7d5feb229964b3a352
faith816-del <faithhosea816@gmail.com>
2025-10-20 21:13:44 +0300

    Fix Tasks import for production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Tasks from "./pages/Tasks";
+import Tasks from "./pages/task";
 
 function App() {
   return (
